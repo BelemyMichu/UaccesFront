48861e55b605b4de14dd2a46cdbbd4d490b8e9af
Sunight01 <kevin.bmorillas@gmail.com>
2024-11-19 18:37:38 -0300

    feat: Login Part 2

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -38,17 +38,17 @@ function LoginPage() {
   return (
     <AuthTemplate>
       <div className="flex items-center justify-center h-screen bg-purple-200">
-        <div className="flex shadow-lg rounded-lg overflow-hidden max-w-4xl">
+        <div className="flex sm:max-lg:flex-col shadow-lg rounded-lg overflow-hidden lg:max-w-4xl">
           {/* Sección de bienvenida */}
-          <div className="w-1/2 bg-purple-300 p-10 flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          <div className="w-1/2 sm:max-lg:w-full bg-purple-300 p-10 flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold text-gray-800 mb-4 text-center">
               ¡Bienvenido a Uaccess!
             </h1>
             <img src="/image_12_1.png" alt="Lock Icon" className="w-200 h-20" />
           </div>
 
           {/* Formulario de inicio de sesión */}
-          <div className="w-1/2 bg-white p-10 flex flex-col justify-center">
+          <div className="w-1/2 sm:max-lg:w-full bg-white p-10 flex flex-col justify-center">
             {!selectedBtn && (
               <div className="flex flex-col space-y-4 gap-4">
                 <h2 className="text-2xl font-semibold text-gray-800 mb-4">
