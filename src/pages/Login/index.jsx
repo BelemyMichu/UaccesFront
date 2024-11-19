c2e32153123a771b556829fec4eff8736b6e065c
Sunight01 <kevin.bmorillas@gmail.com>
2024-11-19 18:13:38 -0300

    feat: Login Part 1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AuthTemplate from "../../components/templates/AuthTemplate";
+import { Html5QrcodeScanner } from 'html5-qrcode';
 import { Link } from "react-router-dom";
 
 function LoginPage() {
+  const [selectedBtn, setSelectedBtn] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [qrResult, setQrResult] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,6 +16,25 @@ function LoginPage() {
     push;
   };
 
+  useEffect(() => {
+    if (selectedBtn === "qrButton" && document.getElementById("reader")) {
+      const success = (result) => {
+        setQrResult(result);
+        scanner.clear();
+      };
+  
+      const error = (err) => {
+        console.warn(err);
+      };
+  
+      const scanner = new Html5QrcodeScanner(
+        "reader",
+        { fps: 10, qrbox: 120 },
+        true
+      );
+      scanner.render(success, error);
+    }
+  }, [selectedBtn]);
   return (
     <AuthTemplate>
       <div className="flex items-center justify-center h-screen bg-purple-200">
@@ -27,46 +49,97 @@ function LoginPage() {
 
           {/* Formulario de inicio de sesión */}
           <div className="w-1/2 bg-white p-10 flex flex-col justify-center">
-            <h2 className="text-2xl font-semibold text-gray-800 mb-6">
-              Inicio de Sesión
-            </h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Correo:
-                </label>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                />
+            {!selectedBtn && (
+              <div className="flex flex-col space-y-4 gap-4">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                  Iniciar como:
+                </h2>
+                <button
+                  onClick={() => setSelectedBtn("qrButton")}
+                  className="bg-purple-500 text-white px-2 py-2 font-semibold rounded-xl hover:bg-purple-600 transition-colors"
+                >
+                  Profesor o Mantención
+                </button>
+                <button
+                  onClick={() => setSelectedBtn("adminButton")}
+                  className="bg-purple-500 text-white px-4 py-2 font-semibold rounded-xl hover:bg-purple-600 duration-200"
+                >
+                  Administración
+                </button>
               </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Contraseña:
-                </label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                />
+            )}
+
+            {selectedBtn === "adminButton" && (
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                  Iniciar sesión
+                </h2>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Correo:
+                  </label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Contraseña:
+                  </label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  />
+                </div>
+                <div className="text-right text-sm">
+                  <a href="#" className="text-purple-500 hover:underline">
+                    ¿Olvidaste tu contraseña?
+                  </a>
+                </div>
+                <div className="flex  flex-row justify-around flex-wrap">
+                  <button
+                    onClick={() => setSelectedBtn("")}
+                    className="bg-purple-500 text-white px-2 py-2 font-semibold rounded-xl hover:bg-purple-600 transition-colors"
+                  >
+                    Volver
+                  </button>
+                  <Link
+                    to="/lectorExcel"
+                    className="bg-purple-500 text-white px-2 py-2 font-semibold rounded-xl hover:bg-purple-600 transition-colors"
+                  >
+                    Iniciar Sesión
+                  </Link>
+                </div>
+              </form>
+            )}
+
+            {selectedBtn === "qrButton" && (
+              <div className="flex flex-col space-y-4 gap-4">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                  Escanea el QR del carnet para habilitar sala
+                </h2>
+                <div>
+                  {qrResult ? (
+                    <a href={qrResult}>Ir a sitio</a>
+                  ) : (
+                    <div id="reader"></div>
+                  )}
+                </div>
+                <button
+                  onClick={() => setSelectedBtn("")}
+                  className="bg-purple-500 text-white px-2 py-2 font-semibold rounded-xl hover:bg-purple-600 transition-colors"
+                >
+                  Volver
+                </button>
               </div>
-              <div className="text-right text-sm">
-                <a href="#" className="text-purple-500 hover:underline">
-                  ¿Olvidaste tu contraseña?
-                </a>
-              </div>
-              <Link
-                to="/lectorExcel"
-                className="w-full py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors"
-              >
-                Iniciar Sesión
-              </Link>
-            </form>
+            )}
           </div>
         </div>
       </div>
