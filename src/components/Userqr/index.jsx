34325a16b6cf76e2deeccd7234ca0484bec5f090
Sunight01 <kevin.bmorillas@gmail.com>
2024-11-29 22:11:07 -0300

    feat: Asistencia page fixed

--- src/components/Userqr/index.jsx
+++ src/components/Userqr/index.jsx
@@ -1,50 +1,73 @@
 import { useEffect, useState } from "react";
 
 import { getUserByRUTAPI } from "../../services/supabase/users";
-import { AddAcademicByQR } from "../../services/supabase/academic";
+import { createAsistencia } from "../../services/supabase/asistencia";
+import { getAcademicByRut } from "../../services/supabase/academic";
 
 const Userqr = ({ rut, sala, sede }) => {
   const [user, setUser] = useState([]);
-  function formatFecha(dateStr) {
-    // Convierte el string de fecha a un objeto Date
-    const date = new Date(dateStr);
 
-    // Obtiene el día de la semana (en español)
+  const formatFecha = () => {
+    const dateNow = new Date();
+  
     const diasSemana = ["Dom", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"];
-    const diaSemana = diasSemana[date.getDay()]; // Obtiene el nombre del día
-
-    // Obtiene la hora y los minutos
-    const horas = date.getHours();
-    const minutos = date.getMinutes().toString().padStart(2, "0"); // Asegura que los minutos sean de dos dígitos
-
-    // Calcula la hora de fin (suponiendo una duración de 45 minutos)
-    const horaFin = new Date(date);
-    horaFin.setMinutes(horaFin.getMinutes() + 45);
-    const horasFin = horaFin.getHours();
-    const minutosFin = horaFin.getMinutes().toString().padStart(2, "0");
-
-    // Formatea el resultado
-    return `${diaSemana} ${horas}:${minutos} - ${horasFin}:${minutosFin}`;
+    const diaSemana = diasSemana[dateNow.getDay()]; // Obtiene el nombre del día
+  
+    // Asegura que las horas, minutos y segundos tengan dos dígitos
+    const horas = String(dateNow.getHours()).padStart(2, '0'); // Hora (00-23)
+    const minutos = String(dateNow.getMinutes()).padStart(2, '0'); // Minutos (00-59)
+    const segundos = String(dateNow.getSeconds()).padStart(2, '0'); // Segundos (00-59)
+  
+    const timeNow = `${horas}:${minutos}:${segundos}`; // Formato "HH:mm:ss"
+    
+    const dayTime = {
+      diaSemana: diaSemana,
+      hora: timeNow
+    };
+  
+    return dayTime;
   }
+  
 
   const getUserByRUT = async (rut) => {
     try {
       const res = await getUserByRUTAPI(rut);
+      if (res.length === 0) {
+        alert("Usuario no encontrado");
+        window.location.reload();
+      }
       console.log(res[0]);
       setUser(res[0])
       if (res[0].rol === "Profesor") {
-        // Hacer que se actualice el codigo para que muestre "presente" en asistencia
+        const resAcad = await getAcademicByRut(rut, sala, sede);
+        resAcad && await createAsistencia({
+          rut: rut,
+          nombre: res[0].nombre,
+          rol: res[0].rol,
+          nrc: resAcad[0].NRC,
+          asignatura: resAcad[0]["Nombre Asignatura"],
+          sede: sede,
+          sala: sala,
+          dia: resAcad[0].Día,
+          hora_inicio: resAcad[0]["Hora Inicio"],
+          hora_final: resAcad[0]["Hora Fin"]
+        })
       } else {
-        const timestamp = new Date().toISOString();
-        const horario = formatFecha(timestamp);
+        const horario = formatFecha();
         console.log(horario);
-        const resp = await AddAcademicByQR(
-          rut,
-          res[0].nombre,
-          sala,
-          sede,
-          timestamp,
-          horario
+        const resp = await createAsistencia(
+          {
+            rut: rut,
+            nombre: res[0].nombre,
+            rol: res[0].rol,
+            nrc: null,
+            asignatura: null,
+            sede: sede,
+            sala: sala,
+            dia: horario.diaSemana,
+            hora_inicio: horario.hora,
+            hora_final: null
+          }
         );
         console.log(resp);
       }
